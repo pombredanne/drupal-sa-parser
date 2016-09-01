9d6afbd22c75984208d10fce8603736a8f592a16
shaisachs <ssachs@ngpvan.com>
2016-09-01 10:27:04 -0400

    Add test framework
    Signed-off-by: shaisachs <ssachs@ngpvan.com>

--- read_sa_page.php
+++ read_sa_page.php
@@ -3,24 +3,41 @@
 include('selector.inc');
 include('ParserChain.inc');
 
-$page = 'https://www.drupal.org/node/2670636';
+function parseSecurityAdvisory($url) {
+	$source = @file_get_contents($url);
 
-$source = @file_get_contents($page);
+	$dom = new SelectorDom($source);
+	$chain = new ParserChain();
 
-$dom = new SelectorDom($source);
-$chain = new ParserChain();
-
-$bullets = $dom->select('.content ul:first-child li');
-$dataPoints = array();
-
+	$bullets = $dom->select('.content ul:first-child li');
+	$dataPoints = array();
 
 
-foreach ($bullets as $bullet) {
-  $data = $bullet['text'];
-  list($key, $value) = $chain->parseBulletText($data);
-  if ($key) {
-    $dataPoints[$key] = $value;
-  }
+
+	foreach ($bullets as $bullet) {
+	  $data = $bullet['text'];
+	  list($key, $value) = $chain->parseBulletText($data);
+	  if ($key) {
+	    $dataPoints[$key] = $value;
+	  }
+	}
+
+	return $dataPoints;	
 }
 
-var_dump($dataPoints);
+function testParse() {
+	$dataPoints = parseSecurityAdvisory('https://www.drupal.org/node/2670636');
+
+	assert($dataPoints['advisoryId'] == 'DRUPAL-SA-CONTRIB-2016-007', 'Advisory ID');
+	assert($dataPoints['version'][0] == '7.x', 'Version - first');
+	assert($dataPoints['version'][1] == '8.x', 'Version - second');
+
+	return true;
+}
+
+if (testParse()) {
+	print "Hooray!\n";
+}
+else {
+	print "Boo\n";
+}
